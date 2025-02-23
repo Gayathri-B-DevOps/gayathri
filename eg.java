fbd43954df2b797f7c868e59576cfe6a32f2f8e4
Gayathri-B-DevOps <143082139+Gayathri-B-DevOps@users.noreply.github.com>
2025-02-24 00:26:38 +0530

    do while

---
+++ eg.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class eg{
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        int count =0;
+        do{
+            System.out.println("enter a number> 10");
+            count= scan.nextInt();
+
+        }
+        while(count <= 10);
+        {
+            
+        }
+    }
+}
