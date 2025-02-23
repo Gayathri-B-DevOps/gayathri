88c3ad5410b16c1350140c786eb3cf9293bb4344
Gayathri-B-DevOps <143082139+Gayathri-B-DevOps@users.noreply.github.com>
2025-02-24 01:14:05 +0530

    functions

---
+++ fun.java
@@ -0,0 +1,15 @@
+public class fun {
+    void iphone()
+    {
+        System.out.println("this is iphone");
+    }
+    void vivo()
+    {
+        System.out.println("this is vivo");
+    }
+    public static void main(String[] args) {
+        fun obj1 = new fun();
+        obj1.iphone();
+        obj1.vivo();
+    }
+}
