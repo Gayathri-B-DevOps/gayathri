794ef72052987349875ec8c13ba8b104bc721158
Gayathri-B-DevOps <143082139+Gayathri-B-DevOps@users.noreply.github.com>
2025-02-24 01:00:57 +0530

    class and object

---
+++ classobject.java
@@ -0,0 +1,11 @@
+public class classobject {
+    String name = "";
+    
+    // string dept = "";
+    int id= 234;
+    public static void main(String[] args) {
+        classobject m1 = new classobject();
+        System.out.println(m1.id);
+
+    }
+}
